1d741f4d8b4f1f26f07bfc3ea27807265d71c481
Alexandru Niculae <alex2august@gmail.com>
2015-08-21 12:18:49 +0300

    remove socket.io connection log

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ app.get('/api/transactions', function(req, res){
 	});
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
